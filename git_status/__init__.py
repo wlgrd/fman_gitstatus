cfebb9b3be2d62eaa81545984ca03f75435faee5
Christian Wilgaard <christian.wilgaard@gmail.com>
2019-08-10 00:41:37 +0200

    Initial working version
    Adds pydocs, removed keybindings and got it to a stable state.

--- git_status/__init__.py
+++ git_status/__init__.py
@@ -1,7 +1,15 @@
-import os, sys
-from fman import DirectoryPaneCommand, DirectoryPaneListener
-from fman import show_alert, show_status_message 
+'''
+	This plugin is only tested on a Windows environment. 
+	Add this repository to ~/AppData/Roaming/fman/Plugins/User/Git Status
+	and the branch name you are working on will be displayed when navigated
+	to the top level directory of any git repository.
+'''
 
+import os, sys
+from fman import DirectoryPaneListener
+from fman import show_status_message 
+
+# Need to add the modules locally to get the plugin to work
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(
     os.path.realpath(__file__))), "git"))
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(
@@ -11,20 +19,22 @@ sys.path.append(os.path.join(os.path.dirname(os.path.dirname(
 	
 from git import Repo
 
-repo = Repo("~/AppData/Roaming/fman/Plugins/User/Git Status")
-branch = repo.active_branch
-
-class SayHi(DirectoryPaneCommand):
-	def __call__(self):
-		show_status_message(branch.name)
-
 class VerifyGitRepo(DirectoryPaneListener):
+	''' 
+		This method listens for directory changes in the pane,
+		checks if we are currently in a directory containing a .git
+		and acts accordingly
+	'''
 	def on_path_changed(self):
-		current_path = self.pane.get_path()
+		# Some times, get_path returns two paths, but the last is always correct
 		try:
-			repo  = Repo(current_path).git_dir
-			branch = repo.active_branch
-			show_status_message(branch.name)
-        	
+			current_path = self.pane.get_path().split("file://")[1]
 		except:
-			show_status_message("No active git repository")
+			current_path = self.pane.get_path().split("file://")
+		try:
+			# This will throw an exception if it is not a repository here
+			repo = Repo(current_path)
+			branch_name = repo.active_branch
+			show_status_message(branch_name.name)
+		except:
+			show_status_message("Not at a top level git repository")
